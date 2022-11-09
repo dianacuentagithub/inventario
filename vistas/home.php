1b4d4df640f84f0069c8c01b533ae57d44030c49
dianacuentagithub <117274626+dianacuentagithub@users.noreply.github.com>
2022-11-09 12:00:26 +0100

    Add files via upload

---
+++ vistas/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/homeStyle.css">
+  <title>home</title>
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="jimena">
+        <div class="row">
+          <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9" id="texto">
+            <h1 class="text-center hi">Hello...
+              <link rel="stylesheet" href="./css/homeStyle.css">
+            </h1>
+            <div class="div-center underline">
+            </div>
+            <p class="text-center intro">  This is the view of HOME</p>
+            <!--<p class="text-center nombre"> Miguel Espeso </p>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
